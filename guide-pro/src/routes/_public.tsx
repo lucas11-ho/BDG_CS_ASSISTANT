fbd76869f7105d4e0366bb1137c39dca52c1de35
lucas11-ho <lacus.mm.ph@gmail.com>
2026-09-15 09:42:42 -0700

    Track public Guide page views and presence

--- guide-pro/src/routes/_public.tsx
+++ guide-pro/src/routes/_public.tsx
@@ -1,10 +1,23 @@
-import { Outlet, createFileRoute } from "@tanstack/react-router";
+import { useEffect } from "react";
+import { Outlet, createFileRoute, useLocation } from "@tanstack/react-router";
 import { PublicLayout } from "@/components/public/PublicLayout";
+import { startPublicTrafficHeartbeat, trackPublicPageView } from "@/lib/traffic-analytics";
 
 export const Route = createFileRoute("/_public")({
-  component: () => (
+  component: PublicRoot,
+});
+
+function PublicRoot() {
+  const location = useLocation();
+
+  useEffect(() => startPublicTrafficHeartbeat(), []);
+  useEffect(() => {
+    trackPublicPageView();
+  }, [location.pathname, location.searchStr]);
+
+  return (
     <PublicLayout>
       <Outlet />
     </PublicLayout>
-  ),
-});
+  );
+}
